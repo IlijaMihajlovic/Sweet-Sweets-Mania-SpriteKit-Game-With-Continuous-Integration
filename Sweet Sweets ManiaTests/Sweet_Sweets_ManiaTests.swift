ef718a662782dfd821a486577bf3c72a40668fc3
IlijaMihajlovic <ikacdrake619@gmail.com>
2019-05-24 12:28:58 +0200

    Implement Unit Tests to project

--- Sweet Sweets ManiaTests/Sweet_Sweets_ManiaTests.swift
+++ Sweet Sweets ManiaTests/Sweet_Sweets_ManiaTests.swift
@@ -17,12 +17,12 @@ class Sweet_Sweets_ManiaTests: XCTestCase {
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        // Put teardown code here. This method is called after the invocation of each test method in the class
     }
 
     func testExample() {
         // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Use XCTAssert and related functions to verify your tests produce the correct results
     
         
     }
