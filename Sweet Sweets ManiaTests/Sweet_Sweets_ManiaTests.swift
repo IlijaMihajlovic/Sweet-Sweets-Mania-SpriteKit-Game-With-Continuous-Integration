b1c158791ca88f5b0c314f474b626b4b94936c8d
IlijaMihajlovic <ikacdrake619@gmail.com>
2019-05-24 14:23:33 +0200

    Check continuous integration with Jenkins and Fastlane. Also working on Unit Tests

--- Sweet Sweets ManiaTests/Sweet_Sweets_ManiaTests.swift
+++ Sweet Sweets ManiaTests/Sweet_Sweets_ManiaTests.swift
@@ -8,25 +8,43 @@
 
 import XCTest
 
+@testable import Sweet_Sweets_Mania
+
+
+//Sweet_Sweets_Mania
 
 class Sweet_Sweets_ManiaTests: XCTestCase {
-    
+    var userProf: UserProfileScene!
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        userProf = UserProfileScene()
+        userProf.addNodes()
+        userProf.setupNodes()
+        
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class
+      
+        
+        userProf = nil
+        super.tearDown()
+        
     }
 
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results
+        XCTAssertNil(userProf.setupNodes)
     
+       
         
     }
 
+    
+    
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
