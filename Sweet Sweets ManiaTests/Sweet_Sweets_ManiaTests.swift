0559a180854e68732e18b02e019d9402cef8458b
IlijaMihajlovic <ikacdrake619@gmail.com>
2019-05-24 12:05:14 +0200

    Implement Unit Tests to project

---
+++ Sweet Sweets ManiaTests/Sweet_Sweets_ManiaTests.swift
@@ -0,0 +1,37 @@
+//
+//  Sweet_Sweets_ManiaTests.swift
+//  Sweet Sweets ManiaTests
+//
+//  Created by Ilija Mihajlovic on 5/24/19.
+//  Copyright © 2019 Ilija Mihajlovic. All rights reserved.
+//
+
+import XCTest
+
+
+class Sweet_Sweets_ManiaTests: XCTestCase {
+    
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+        
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
